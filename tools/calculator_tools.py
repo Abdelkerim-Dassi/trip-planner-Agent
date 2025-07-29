5891f1baee8acef6b5b37f61ee1e2efa31751baa
Abdelkerim Dassi <abdelkerymdessi@gmail.com>
2025-07-29 17:05:16 +0100

    optimised search_tools

---
+++ tools/calculator_tools.py
@@ -0,0 +1,17 @@
+from langchain.tools import tool
+
+class CalculatorTool:
+    @tool("Make a calculation")
+    def calculate(self,operation: str) -> str:
+        """
+        Useful to perform any mathematical calculations,
+        like sum,minus,multiplication,diviion,etc.
+        The input to this tool should be a valid mathematical expression,
+        such as '2+2', '200*7', '10/2', or '5000/2*10'
+        """
+        try:
+            return eval(operation)
+        except SyntaxError:
+            return f"Error calculating '{operation}': Invalid syntax in mathematical expression."
+        except ZeroDivisionError:
+            return f"Error calculating '{operation}': Division by zero is not allowed."
