7eba94f7ef402659f984d52fac8eeeee15759a59
Abdelkerim Dassi <abdelkerymdessi@gmail.com>
2025-07-29 16:47:42 +0100

    finalised tasks

--- tasks.py
+++ tasks.py
@@ -7,35 +7,72 @@ class CustomTasks:
     def __tip_section(self):
         return "If you do your BEST WORK, I'll give you a $10,000 commission!"
 
-    def task_1_name(self, agent, var1, var2):
+    def plan_itinerary(self, agent, city, travel_dates, interests):
         return Task(
             description=dedent(
                 f"""
-            Do something as part of task 1
-            
-            {self.__tip_section()}
+            ***Task**: Develop a 7-day travel itinerary for {city}.*
+            **Description**: Expand the city guide into a full 7-day travel itinerary with detailed per-day plans, including weather forecasts, places to eat, packing suggestions, and a budget breakdown.
+            You MUST suggest actual places to visit, actual hotels to stay, and actual restaurants to go to.
+            This itinerary should cover all aspects of the trip, from arrival to departure, integrating the city guide information with practical travel logistics.
+            Your final answer MUST be a complete expanded travel plan, formatted as markdown, encompassing a daily schedule, anticipated weather conditions, recommended clothing and items to pack, and a detailed budget, ensuring THE BEST TRIP EVER.
+            Be specific and give it a reason why you picked each place, what makes them special!
+            **Parameters**:
+            - City: {city}
+            - Travel Dates: {travel_dates}
+            - Interests: {interests}
+
+            **Note**: {self.__tip_section()}
     
-            Make sure to use the most recent data as possible.
-    
-            Use this variable: {var1}
-            And also this variable: {var2}
+            Make sure to use the most recent data as possible, and consider the current travel restrictions or requirements.
         """
             ),
             expected_output="The expected output of the task",
             agent=agent,
         )
 
-    def task_2_name(self, agent):
+
+    def identify_city(self, agent,origin, destination, travel_dates, interests):
         return Task(
             description=dedent(
                 f"""
-            Take the input from task 1 and do something with it.
-                                       
-            {self.__tip_section()}
+            ***Task**: Identify the city for the travel itinerary.*
+            **Description**: Analyze and select the most suitable city for a 7-day travel itinerary based on specific
+            criteria such as weather patterns, seasonal events, and travel costs.
+            This task involves comparing multiple cities considering factors like current weather conditions,upcoming events,and overall travel costs.
+            **Parameters**:
+            - Origin: {origin}
+            - Cities: {destination}
+            - Travel Date: {travel_dates}
+            - Interests: {interests} 
 
-            Make sure to do something else.
+           **Note**:{self.__tip_section()}
+
         """
             ),
-            expected_output="The expected output of the task",
+            expected_output="Detailed report on the chosen city including flight costs, weather forecast, and attractions",
             agent=agent,
+        )
+
+
+def gather_city_info(self, agent,city, travel_date, interests):
+        return Task(
+            description=dedent(
+                f"""
+            ***Task**: Gather In-Depth City Information.
+            **Description**: Compile an in-depth guide for the selected city, focusing on key attractions, local customs, special events, and daily activity recommendations.
+            The guide should provide a thorough overview of what the city has to offer, including hidden gems, cultural hotspots, must-visit landmarks, weather forecasts, and high-level costs.
+            This guide should be tailored to enhance the travel experience, providing practical tips and cultural insights.
+            This guide should be know the must-visit landmarks, hidden gems, cultural hotspots, and practical travel tips.
+            The final answer must be a comprehensive city guide.
+            **Parameters**:
+            - Cities: {city}
+            - Travel Date: {travel_date}
+            - Interests: {interests} 
+
+           **Note**:{self.__tip_section()}
+
+        """
+            ),
+            agent=agent
         )
